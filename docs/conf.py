68d5d0fc3f7329c629dea13065b9c61bf145ae82
Arun Chahar <110096566+arun2022cybersec@users.noreply.github.com>
2024-06-15 18:47:17 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,9 +18,9 @@ sys.path.insert(0, os.path.abspath("../src/"))
 
 # -- Project information -----------------------------------------------------
 
-project = "ai-python docs"
-copyright = "2022, Daniel Ciborowski"
-author = "Daniel Ciborowski"
+project = "python_logic_simulator"
+copyright = "2024, Arun Chahar"
+author = "Arun Chahar"
 
 # The full version, including alpha/beta/rc tags
 release = "0.1.0"
